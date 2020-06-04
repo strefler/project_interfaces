cb0e589b99e64ee51ae5ce628638e7cc03b85efa
Alois Dirnaichner <alodi@directbox.com>
2020-06-04 16:46:06 +0200

    Improve the output script.

--- ar6/produce_output.R
+++ ar6/produce_output.R
@@ -1,9 +1,12 @@
 require(data.table)
 
-## replace the model name and produce output for all MIF files in directory
+## replace the model name with MODEL and produce output for all MIF files in directory
+## based on the MAPPING
+## the output file gets OUTPUT_PREFIX prepended to the file name.
 
 MODEL <- "REMIND-EDGET 2.1"
 MAPPING <- "~/git/project_interfaces/ar6/mapping_r21m42_AR6DB.csv"
+OUTPUT_PREFIX <- "AR6_"
 
 set_model <- function(mif, model){
   dt <- fread(mif, header=T)
@@ -14,7 +17,7 @@ set_model <- function(mif, model){
 flist <- list.files(".", "*.mif")
 for(fl in flist){
   set_model(fl, MODEL)
-  iamc::write.reportProject(fl, MAPPING, paste0("AR6_", fl))
+  iamc::write.reportProject(fl, MAPPING, paste0(OUTPUT_PREFIX, fl))
 }
 
 
