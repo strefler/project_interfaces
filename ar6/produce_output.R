01b3268933f48c7e206e2743d620be9780a8d334
Bjoern Soergel <16190669+bs538@users.noreply.github.com>
2020-06-12 14:09:00 +0200

    added option to shorten scenario name to produce_output.R

--- ar6/produce_output.R
+++ ar6/produce_output.R
@@ -13,16 +13,18 @@ LOGFILE <- "missing.log"
 
 MIF_DIRECTORY <- "."
 OUTPUT_DIRECTORY <- "AR6_output/"
+REMOVE_FROM_SCEN <- "C_"
 
-set_model <- function(mif, model){
+set_model_and_scenario <- function(mif, model, scen_remove = NULL){
   dt <- fread(mif, header=T)
   dt[, Model := model]
+  if (!is.null(scen_remove)) dt[, Scenario := gsub(scen_remove,"",Scenario)]
   fwrite(dt, mif, sep=";")
 }
 
 flist <- list.files(MIF_DIRECTORY, "*.mif")
 for(fl in flist){
-  set_model(fl, MODEL)
+  set_model_and_scenario(fl, MODEL, REMOVE_FROM_SCEN)
   iamc::write.reportProject(fl, MAPPING,
                             file.path(OUTPUT_DIRECTORY, paste0(OUTPUT_PREFIX, fl)),
                             missing_log=LOGFILE)
