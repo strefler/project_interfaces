7c24378ef8abc31e376c69b5bc9d6b97f63e3438
Alois Dirnaichner <alodi@directbox.com>
2020-06-12 11:51:08 +0200

    Improve output generation script, allow for output directory to be specified.

--- ar6/produce_output.R
+++ ar6/produce_output.R
@@ -1,18 +1,18 @@
 require(data.table)
-require(devtools)
-load_all("~/git/iamc")
+require(iamc)
 
 ## replace the model name with MODEL and produce output for all MIF files in directory
 ## based on the MAPPING
 ## the output file gets OUTPUT_PREFIX prepended to the file name.
 ## Warnings are appended to the given logfile.
 
-MODEL <- "REMIND-EDGE-Transport 2.1"
-MAPPING <- "~/git/project_interfaces/ar6/mapping_r21m42_AR6DB.csv"
+MODEL <- "REMIND-MAgPIE 2.1-4.2"
+MAPPING <- "mapping_r21m42_AR6DB.csv"
 OUTPUT_PREFIX <- "AR6_"
 LOGFILE <- "missing.log"
 
 MIF_DIRECTORY <- "."
+OUTPUT_DIRECTORY <- "AR6_output/"
 
 set_model <- function(mif, model){
   dt <- fread(mif, header=T)
@@ -23,7 +23,7 @@ set_model <- function(mif, model){
 flist <- list.files(MIF_DIRECTORY, "*.mif")
 for(fl in flist){
   set_model(fl, MODEL)
-  iamc::write.reportProject(fl, MAPPING, paste0(OUTPUT_PREFIX, fl), warning_log=LOGFILE)
+  iamc::write.reportProject(fl, MAPPING,
+                            file.path(OUTPUT_DIRECTORY, paste0(OUTPUT_PREFIX, fl)),
+                            missing_log=LOGFILE)
 }
-
-
