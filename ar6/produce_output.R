d281a9754ee5bec0b1f59131aec61351d995e125
Alois Dirnaichner <alodi@directbox.com>
2020-06-15 16:30:58 +0200

    Create output dir if non-existent. Avoid prepending scenario prefixes multiple times.

--- ar6/produce_output.R
+++ ar6/produce_output.R
@@ -6,23 +6,30 @@ require(iamc)
 ## the output file gets OUTPUT_PREFIX prepended to the file name.
 ## Warnings are appended to the given logfile.
 
-MODEL <- "REMIND-MAgPIE 2.1-4.2"
-MAPPING <- "mapping_r21m42_AR6DB.csv"
+MODEL <- "REMIND-Transport 2.1"
+MAPPING <- "~/git/project_interfaces/ar6/mapping_r21m42_AR6DB.csv"
 OUTPUT_PREFIX <- "AR6_"
 LOGFILE <- "missing.log"
 
 MIF_DIRECTORY <- "."
 OUTPUT_DIRECTORY <- "AR6_output/"
-REMOVE_FROM_SCEN <- "C_"
-ADD_TO_SCEN <- "SusDev_"
+REMOVE_FROM_SCEN <- NULL
+ADD_TO_SCEN <- "Transport_"
+
+if(!file.exists(OUTPUT_DIRECTORY)){
+  dir.create(OUTPUT_DIRECTORY)
+}
 
 set_model_and_scenario <- function(mif, model, scen_remove = NULL, scen_add = NULL){
   dt <- fread(mif, header=T)
   dt[, Model := model]
   if (!is.null(scen_remove)) dt[, Scenario := gsub(scen_remove,"",Scenario)]
   if (!is.null(scen_add)) {
-     print("careful, will add prefix multiple times if rerun")
-     dt[, Scenario := paste0(scen_add,Scenario)]
+    if(grepl(scen_add, unique(dt$Scenario), fixed=TRUE)){
+      print(sprintf("Prefix %s already found in scenario name in %s.", scen_add, mif))
+    }else{
+      dt[, Scenario := paste0(scen_add,Scenario)]
+    }
   }
   fwrite(dt, mif, sep=";")
 }
