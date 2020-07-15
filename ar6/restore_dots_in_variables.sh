0b5f77641812863b2cf2bc5048be0939b35a5583
Bjoern Soergel <16190669+bs538@users.noreply.github.com>
2020-07-15 18:22:20 +0200

    replace N/A with blank in bash script

--- ar6/restore_dots_in_variables.sh
+++ ar6/restore_dots_in_variables.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "restoring PM2.5 and poverty w.r.t. median income dots in variable names"
 sed 's/wrt median income/w.r.t. median income/g' ./AR6_output/SusDev_alldata.mif > ./AR6_output/temp.mif
-sed 's/PM2\_5/PM2.5/g' ./AR6_output/temp.mif > ./AR6_output/SusDev_alldata_final.mif
+sed -i 's/PM2\_5/PM2.5/g' ./AR6_output/temp.mif 
+echo "replacing N/A for missing years with blanks as recommended by Ed Byers"
+sed 's/N\/A//g' ./AR6_output/temp.mif > ./AR6_output/SusDev_alldata_final.mif
 rm ./AR6_output/temp.mif
