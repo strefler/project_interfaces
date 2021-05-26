65a87b127688633f8abdfceede6ebf1596422aa4
Anastasis Giannousakis <11047746+giannou@users.noreply.github.com>
2021-05-26 11:30:07 +0200

    add relative differences column in the summation checks

--- checkSummations.R
+++ checkSummations.R
@@ -11,4 +11,5 @@ names(check_variables) <- gsub(" [1-9]$","",names(check_variables))
 tmp2<-NULL
 for (i in names(check_variables)) tmp2<-rbind(tmp2,check_quitte(data,check_variables[i]))
 tmp2$diff<-abs(tmp2$sum.value-tmp2$value)
-write.table(arrange(tmp2,desc(diff)),sep=";",file = "checkSummations.log",quote = F,row.names = F)
+tmp2$rel.diff<-100*abs((tmp2$sum.value-tmp2$value)/tmp2$value)
+write.table(arrange(tmp2,desc(rel.diff)),sep=";",file = "checkSummations.log",quote = F,row.names = F)
